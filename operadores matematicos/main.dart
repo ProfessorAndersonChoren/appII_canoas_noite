ab6f753c5955930dfcc4cb0b8ade0c5816a15688
Anderson Coimbra Choren <andersonchoren@gmail.com>
2023-02-01 19:20:51 -0300

    Operadores matemáticos

---
+++ operadores matematicos/main.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Serve para imprimir algo
+  // print('Hello World!!!');
+
+  var name = 'Anderson';
+  name = 'João';
+  // name = 10;
+
+  dynamic value = 10;
+  value = 'João';
+
+  int age = 31;
+
+  double height = 1.8; // Altura em metros
+
+  height = 100;
+}
