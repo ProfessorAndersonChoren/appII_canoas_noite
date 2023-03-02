981992769443671c160dee3f69a5fdb637eb7c04
Anderson Coimbra Choren <andersonchoren@gmail.com>
2023-03-01 21:54:08 -0300

    Pokedex - Parte 1

--- pokedex/lib/screens/home/home.dart
+++ pokedex/lib/screens/home/home.dart
@@ -1,18 +1,140 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: Image.asset("assets/logo.png"),
-        title: Text("Charmander #004"),
+        title: const Text("Charmander #004"),
       ),
       body: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 20,
+          horizontal: 16,
+        ),
+        width: MediaQuery.of(context).size.width,
         child: Column(
           children: [
             Image.asset("assets/charmander.png"),
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 20,
+              ),
+              child: Text(
+                  "Tem preferência por coisas quentes. Quando chove, diz-se que o vapor jorra da ponta da cauda."),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    // 1° Linha
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            titleText(
+                              text: "Altura",
+                              context: context,
+                            ),
+                            subtitleText(
+                              context: context,
+                              text: "0.6m",
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            titleText(
+                              text: "Peso",
+                              context: context,
+                            ),
+                            subtitleText(
+                              text: "8.5kg",
+                              context: context,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    // 2° Linha
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            titleText(
+                              text: "Tipo",
+                              context: context,
+                            ),
+                            customChip(
+                              text: "Fogo",
+                              backgroundColor: const Color(0xFFF17F2E),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            titleText(
+                              text: "Habilidade",
+                              context: context,
+                            ),
+                            subtitleText(
+                              text: "Chama",
+                              context: context,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  } // Build
+
+  Text titleText({
+    required String text,
+    required BuildContext context,
+  }) {
+    return Text(
+      text,
+      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+    );
+  }
+
+  Text subtitleText({
+    required String text,
+    required BuildContext context,
+  }) {
+    return Text(
+      text,
+      style: Theme.of(context).textTheme.titleMedium,
+    );
+  }
+
+  Chip customChip({
+    required String text,
+    required Color backgroundColor,
+  }) {
+    return Chip(
+      label: Text(text),
+      backgroundColor: backgroundColor,
+      labelStyle: const TextStyle(
+        color: Colors.white,
       ),
     );
   }
